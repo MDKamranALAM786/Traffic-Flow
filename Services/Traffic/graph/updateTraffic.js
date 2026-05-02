c51e095dd80d49099db2c90336890f8e2dc65989
Md Kamran Alam <ka6923442@gmail.com>
2026-05-03 01:44:12 +0530

    Traffic Service Ready to update travel time for surrounding roads based on real time traffic

--- Services/Traffic/graph/updateTraffic.js
+++ Services/Traffic/graph/updateTraffic.js
@@ -5,40 +5,6 @@ import {getDataPoints, getNeighbours, addNewTraffic} from "./callNeo4j.js";
 import {getRealTravelTime} from "./callMapBox.js";
 import {sleep} from "./helper.js";
 
-export const changeTraffic = async (src, dest, newTime) => {
-    let session = driver.session({database : "routing-project"});
-
-    try {
-        const result = await session.run(`
-            MATCH (:Intersection {name:$src})-[r:ROAD]->(:Intersection {name:$dest})
-            WITH r
-            SET r.travel_time = r.travel_time + $newTime
-            RETURN r`,
-            {src : src, dest : dest, newTime : newTime}
-        );
-
-        let hasChanged, message;
-        if(!result.records || result.records.length === 0) {
-            hasChanged = false;
-            message = "No Roads Found!";
-        } else {
-            hasChanged = true;
-            message = "Travel Time changed Successfully!";
-        }
-
-        let finalResult = {
-            hasChanged : hasChanged,
-            message : message
-        };
-
-        return(finalResult);
-    } catch(err) {
-        console.log(`Error : ${err.cause}`);
-    } finally {
-        await session.close();
-    }
-};
-
 export const updateAllTraffic = async (congestionLevels) => {
     let session = driver.session({database : "routing-project"});
 
@@ -116,6 +82,7 @@ export const updateRealTrafficData = async (lat, long) => {
 
         while(unprocessedEdges.size !== 0) {
             console.log(`Processing chunk ${++chunkCount}`);
+
             // pick 10 unique nodes
             for(let edge of unprocessedEdges) {
                 let {node1, node2} = neighbourPairs[edge];
