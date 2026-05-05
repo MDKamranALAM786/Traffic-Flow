2269da804dff415fe1a6051fbe70f8a845a8dcb0
Md Kamran Alam <ka6923442@gmail.com>
2026-05-06 01:17:39 +0530

    Frontend base setup before starting with its development

---
+++ Frontend/src/context/AuthContext.jsx
@@ -0,0 +1,68 @@
+import axios from "axios";
+import httpStatus from "http-status";
+import {createContext, useContext, useState} from "react";
+
+export const AuthContext = createContext({});
+
+const BASE_URL = import.meta.env.GATEWAY_URL;
+const client = axios.create({
+    baseURL : BASE_URL
+});
+
+export const AuthProvider = ({children}) => {
+    let [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const handleSignUp = async (name, username, email, password) => {
+        try {
+            let data = {name, username, email, password};
+            let res = await client.post("/signup", data);
+
+            const accessToken = res.data.token;
+            localStorage.setItem("accessToken", accessToken);
+            setIsAuthenticated(true);
+
+            return({
+                success : true,
+                message : res.data.message
+            });
+        } catch(err) {
+            return({
+                success : false,
+                message : err.response?.data?.message || "Signup Failed"
+            });
+        }
+    };
+
+    const handleLogin = async (username, password) => {
+        try {
+            let data = {username, password};
+            let res = await client.post("/login", data);
+
+            const accessToken = res.data.token;
+            localStorage.setItem("accessToken", accessToken);
+            setIsAuthenticated(true);
+
+            return({
+                success : true,
+                message : res.data.message
+            });
+        } catch(err) {
+            return({
+                success : false,
+                message : err.response?.data?.message || "Login Failed"
+            });
+        }
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem("accessToken");
+        setIsAuthenticated(false);
+    };
+
+    const data = {isAuthenticated, setIsAuthenticated, handleSignUp, handleLogin, handleLogout};
+    return(
+        <AuthContext.Provider value={data}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
