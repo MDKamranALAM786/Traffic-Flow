51d0fdc1b742c891152c9bd129f1670f1c1a4d2f
Md Kamran Alam <ka6923442@gmail.com>
2026-05-10 19:26:07 +0530

    Landing Page is ready

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import {HashRouter as Router, Routes, Route} from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import LandingPage from "./pages/landing.jsx";
 
 export default function App() {
-  return(
+  return (
     <div>
-      <Router></Router>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
     </div>
   );
 };
